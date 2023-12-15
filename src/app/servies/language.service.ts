87e8410a2718fda06cb13a413bbc636cf49dad40
Mohammed Belhaj <m.belhaj_zakelijk@hotmail.com>
2023-12-15 15:42:13 +0100

    Update language.service.ts

--- src/app/servies/language.service.ts
+++ src/app/servies/language.service.ts
@@ -6,6 +6,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class LanguageService {
+  private supportedLanguages: string[] = ['en', 'nl', 'es'];
   private selectedLanguageSubject: BehaviorSubject<string> =
     new BehaviorSubject<string>('en');
 
@@ -15,26 +16,45 @@ export class LanguageService {
   constructor(
     @Inject(DOCUMENT) private document: Document,
     @Inject(LOCALE_ID) private locale: string
-  ) {}
+  ) {
+    // Extract language from the URL on service initialization
+    this.extractLanguageFromUrl();
+  }
 
   getSelectedLanguage(): string {
     return this.selectedLanguageSubject.value;
   }
 
   setSelectedLanguage(language: string): void {
-    this.selectedLanguageSubject.next(language);
+    if (this.supportedLanguages.includes(language)) {
+      this.selectedLanguageSubject.next(language);
+      this.updateUrlLanguage(language);
+    } else {
+      console.error(`Unsupported language: ${language}`);
+    }
   }
 
-  changeLanguage(newLocale: string): void {
+  private extractLanguageFromUrl() {
     try {
-      this.getSelectedLanguage();
-
       const currentUrl = this.document.location.href;
-      const newUrl = currentUrl.replace(`/${this.locale}/`, `/${newLocale}/`);
+      for (const lang of this.supportedLanguages) {
+        if (currentUrl.includes(`/${lang}/`)) {
+          this.selectedLanguageSubject.next(lang);
+          break;
+        }
+      }
+    } catch (error) {
+      console.error('Error while extracting language from URL:', error);
+    }
+  }
 
+  private updateUrlLanguage(language: string) {
+    try {
+      const currentUrl = this.document.location.href;
+      const newUrl = currentUrl.replace(`/${this.locale}/`, `/${language}/`);
       this.document.location.href = newUrl;
     } catch (error) {
-      console.error('Error while changing language:', error);
+      console.error('Error while updating URL language:', error);
     }
   }
 }
